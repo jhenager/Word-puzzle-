908dcf6d3e40f07d6facaf3665205c22e309b13a
Ian Gregg <iangregg188@gmail.com>
2020-06-09 13:47:03 -0700

    Corrected the loop to include split and join functions to correct the output
    Co-authored-by: James Henager <jameschenager@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -7,15 +7,18 @@ $(document).ready(function(){
   $("form#sentence").submit(function(event) {
     event.preventDefault ();
     const inputSentence = $("input#sentence1").val();
-    
-    String strArray[inputSentence] = str.split("")
 
-    // const arr =["z","e","f","o","z"]
-    // for (let i=0; i<=arr.length;i++) {
-    // if  
-    //   arr[i]=== ("a"||"e"||"o"||"u"||"i")
-    //   arr[i]== ("--")
-    //  //("a"||"e"||"o"||"u"||"i")
-    // }
+    for (let i=0; i<inputSentence.length; i++) {
+      let charArray = inputSentence.split("a").join("--").split("e").join("--").split("i").join("--").split("o").join("--").split("u").join("--")
+      
+      alert(charArray);
+    }
+    //for (let i=0; i<charArray.length; i++) {
+      //if  (charArray[i] === ("a"||"e"||"o"||"u"||"i"))
+        
+        //charArray[i]== ("--")
+      
+      //let 
+    
   });
 });
