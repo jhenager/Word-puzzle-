30da794828eeaf31d07c2a293382682359b1a165
Ian Gregg <iangregg188@gmail.com>
2020-06-09 12:02:01 -0700

    We degugged code and strived to make a DRY approach
    Co-authored-by: James Henager <jameschenager@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,21 @@
-$(wordpuzzle).ready(function(){
-  $("").submit(function(event) {
+// Business Logic
+
+
+
+// UI Logic
+$(document).ready(function(){
+  $("form#sentence").submit(function(event) {
     event.preventDefault ();
+    const inputSentence = $("input#sentence1").val();
     
-    const arr =[z,e,f,o,z]
-    for (let i=0; i<=arr.length;i++) {
-    if  arr[i] = ("a"||"e"||"o"||"u"||"i")
-      arr[i]= ("--")
-      
-    }
+    String strArray[inputSentence] = str.split("")
+
+    // const arr =["z","e","f","o","z"]
+    // for (let i=0; i<=arr.length;i++) {
+    // if  
+    //   arr[i]=== ("a"||"e"||"o"||"u"||"i")
+    //   arr[i]== ("--")
+    //  //("a"||"e"||"o"||"u"||"i")
+    // }
+  });
 });
